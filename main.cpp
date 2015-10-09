68154c4563049a3a70d03b2bff64ef4e6fafb8e5
Stepan Orlov <a@b.com>
2015-10-09 14:45:18 +0300

    Added function that computes square.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
+double sqr(double x)
+{
+    return x*x;
+}
+
 int main()
 {
-    int x[10];
-    cout << x[0] << endl;
-    cout << "Hello World!" << endl;
+    cout << 5*3 << ' ' << 10 << endl;
+    cout << "Hello" << endl;
+    cout << sqr(sqr(1.5)) << endl;
     return 0;
 }
 
