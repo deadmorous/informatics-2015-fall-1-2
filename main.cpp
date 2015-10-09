8f2252f0c6a940b0b3c2b2b2b97c0f9277e3e527
Stepan Orlov <a@b.com>
2015-10-09 15:49:01 +0300

    Implemented Fibonacci number generator.

--- main.cpp
+++ main.cpp
@@ -8,11 +8,17 @@ double sqr(double x)
     return x*x;
 }
 
+int fib(int k)
+{
+    return k==1? 1: k==2? 2: fib(k-1) + fib(k-2);
+}
+
 int main()
 {
     cout << 5*3 << ' ' << 10 << endl;
     cout << "Hello" << endl;
     cout << sqr(sqr(1.5)) << endl;
+    cout << fib(45) << endl;
     return 0;
 }
 
