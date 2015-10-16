3e6c4584d04dcecbe6540c796220cb2204f4f795
Stepan Orlov <a@b.com>
2015-10-16 14:51:48 +0300

    Measured time spent for Fibonacci number calculation.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <cmath>
 
+#include <QTime>
+
 using namespace std;
 
 double sqr(double x)
@@ -10,7 +12,7 @@ double sqr(double x)
 
 int fib(int k)
 {
-    return k==1? 1: k==2? 2: fib(k-1) + fib(k-2);
+    return k<3 ?   k :   fib(k-1) + fib(k-2);
 }
 
 int main()
@@ -18,7 +20,10 @@ int main()
     cout << 5*3 << ' ' << 10 << endl;
     cout << "Hello" << endl;
     cout << sqr(sqr(1.5)) << endl;
+    QTime t;
+    t.start();
     cout << fib(45) << endl;
+    cout << "Time elapsed: " << t.elapsed() << " ms" << endl;
     return 0;
 }
 
