98d030ff952d08b540396bc39c25c26a9988ed90
Stepan Orlov <a@b.com>
2015-11-13 15:11:06 +0300

    Don't remember.

--- main.cpp
+++ main.cpp
@@ -1,57 +1,31 @@
 #include <iostream>
 #include <cmath>
 
-#include <QTime>
-
 using namespace std;
-
-double sqr(double x)
-{
-    return x*x;
-}
-
-int fib(int k)
-{
-    return k<3 ?   k :   fib(k-1) + fib(k-2);
-}
-
-void fib2_(int k, int& ck, int& ck1)
-{
-    if (k <= 2)
-    {
-        ck = k;
-        ck1 = k - 1;
-    }
-    else
-    {
-        // Вычислить с(k-2) и c(k-1)
-        // (это можно сделать, вызвав функцию fib2_)
-        // Сумма с(k-2) и c(k-1) - это c(k)
-        fib2_(k-1, ck1, ck);
-        ck += ck1;
-    }
-}
-
-int fib2(int k)
-{
-    int ck, ck1;
-    fib2_(k, ck, ck1);
-    return ck;
-}
 
 int main()
 {
-//    cout << 5*3 << ' ' << 10 << endl;
-//    cout << "Hello" << endl;
-//    cout << sqr(sqr(1.5)) << endl;
-    QTime t;
-    t.start();
-    cout << fib(45) << endl;
-    cout << "Time elapsed: " << t.elapsed() << " ms" << endl;
+    const int N = 10;
+    int x[N] = {6, 3, 6, 8, 22, -7, 11, 9, 77, 3};
+    // int x[N] = {6, 3, -7, 9, 3, 6, 8, 11, 22, 77};
 
-    t.restart();
-    cout << fib2(45) << endl;
-    cout << "Time elapsed: " << t.elapsed() << " ms" << endl;
+    for (int i=0; i<N; i++)
+        cout << x[i] << endl;
 
-    return 0;
+    cout << "=====" << endl;
+
+
+    for (int ilast=N-1; ilast>1; ilast--)
+        {
+        int imax = 0;
+        for (int i=1; i<=ilast; i++)
+            if (x[i] > x[imax])
+                imax = i;
+        swap(x[imax], x[ilast]);
+        }
+
+//    cout << "Maximum element " << x[imax] << " found at index " << imax << endl;
+    cout << "=====" << endl;
+    for (int i=0; i<N; i++)
+        cout << x[i] << endl;
 }
